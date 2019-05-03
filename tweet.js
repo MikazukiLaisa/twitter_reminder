417d4ed74e8791cf0a55aa116547251067fc1710
MikazukiLaisa <MikazukiLaisa>
2019-05-04 01:34:31 +0900

    fixed update1.0.2

--- tweet.js
+++ tweet.js
@@ -1,9 +1,9 @@
 const Twitter = require('twitter')
 
-const consumer_key = 'oKdF0pmYxeYejFuPE3B93BFM2'
-const consumer_secret = 'tq4a2vrJ9nIelKqxtGbs1X9ylR6m9sxLFbvclEPlKBck5DxxLs'
-const access_token_key = '1671656778-WOIwCPSSeRSQ2MdBz9YPZiIa9wep2LeBdKYXr1R'
-const access_token_secret = 'W1EvTLF2QKNJzYCtOsjVI5vxCAXEP7C9jWBhxmZxb51vE'
+const consumer_key = ''
+const consumer_secret = ''
+const access_token_key = ''
+const access_token_secret = ''
 
 const client = new Twitter({
   consumer_key,
@@ -16,11 +16,11 @@ const client = new Twitter({
 
 
 
-  client.stream( 'statuses/filter', { track : '@Mikazuki_Laisa' }, function( stream ) {
+  client.stream( 'statuses/filter', { track : '@TWITTER ID' }, function( stream ) {
     // フィルターされたデータのストリームを受け取り、ツイートのテキストを表示する
     stream.on( 'data', function( data ) {
         var text = data.text; // ツイートのテキスト
-        var textCleaned = text.replace( /@Mikazuki_Laisa/g, "" ); // アカウント名は不要
+        var textCleaned = text.replace( /@TWITTER ID/g, "" ); // アカウント名は不要
         var destination = '@' + data.user.screen_name;
         console.log("---------------------------------------------------")
         console.log( textCleaned );
@@ -31,20 +31,20 @@ const client = new Twitter({
 
 var reserve = (tweet_message, user) => {
     var result = tweet_message.split(" ");
-    var result_date = result[1].split("/");
+    var result_date = result[0].split("/");
     if(!isNaN(result_date[0])){
       var now = Date.now();
-      var buffer = (result[1] + " " + result[2]).toString();
+      var buffer = (result[0] + " " + result[1]).toString();
       var message_time = new Date(buffer).getTime();
       var df = message_time - now;
   
       console.log(result);
       console.log("booking : " + df);
       console.log("destination : " + user);
-      console.log("booked tweet : " + result[3]);
+      console.log("booked tweet : " + result[2]);
       
       setTimeout(() => {
-          var twitter_status = user + " " + result[3];
+          var twitter_status = user + " " + result[2];
   
               client.post('statuses/update', {status: twitter_status}, function(error, tweet, response) {
                   if (!error) {
